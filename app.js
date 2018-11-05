4eccc798f21ce7308b43bb1ffe10ac6b73179502
louisfromtoronto <31932581+louisfromtoronto@users.noreply.github.com>
2018-11-05 15:36:04 -0500

    added /createdb route. App now interfaces with the db.

--- app.js
+++ app.js
@@ -1,7 +1,29 @@
-const express = require("express");
+// MySQL Connection
 const mysql = require("mysql");
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "password"
+});
+db.connect(err => {
+  if (err) throw err;
+  console.log("Connected!");
+});
 
-const app = express();
-app.listen("30000", () => {
-  console.log("Server started on port 3000");
+// Express routing
+const express = require("express");
+app = express();
+
+app.get("/createdb", (req, res) => {
+  console.log("/createdb");
+  let sql = "CREATE DATABASE nodemysql";
+  db.query(sql, (err, result) => {
+    if (err) throw err;
+    console.log(result);
+    res.send("Database created...");
+  });
+});
+
+app.listen("3000", () => {
+  console.log("Now listening on port 3000");
 });
